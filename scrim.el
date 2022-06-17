6a0a1416b7645f4718a5b892ae5e875e17d6a4dc
Austin Haas <austin@pettomato.com>
2022-06-16 21:34:14 -0700

    Drop snapshot.

--- scrim.el
+++ scrim.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Austin Haas <austin@pettomato.com>
 ;; URL: http://github.com/austinhaas/scrim
-;; Version: 0.0.8-SNAPSHOT
+;; Version: 0.0.8
 
 ;; This file is not part of GNU Emacs.
 
